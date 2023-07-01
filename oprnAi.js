8da9b68a6de9cdf9237280afd236c098a156a850
MURAYAMADESU <kousuke_21n1200380@nnn.ed.jp>
2023-07-01 19:18:35 +0900

    ChatGPTを機能に追加しました。

--- oprnAi.js
+++ oprnAi.js
@@ -1,15 +1,38 @@
 'use Strict'
 // 変数の定義
 //モジュールの読み込み
+const { Configuration, OpenAIApi } = require("openai");
 const dotenv = require('dotenv').config();
 const fs = require('fs');
 const http = require('http');
 
-//httpサーバー
-const server = http.createServer();
+//httpサーバーポートの設定
+const port = 8000;
 
 //環境変数の読み込み
 const env = process.env;
 
+//chatGPTへの入力メッセージ
+const chatGptMessage = "4問のクイズと、その答えを4択で出して";
+
 //テスト出力
-console.log(env.OPENAI_API_KEY);
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+(async () => {
+    const completion = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: [{ role: "user", content: chatGptMessage }],
+    });
+    console.log(completion.data.choices[0].message);
+})();
+
+
+/**
+http.createServer( (request, response)=>{
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    response.end('Hello World');
+}).listen(port);
+*/
