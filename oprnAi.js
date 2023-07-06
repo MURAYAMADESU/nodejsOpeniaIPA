f40c0ad18d051bfa8b4690234f46e6ff89ea4ae4
MURAYAMADESU <murayamadesuhai@gmail.com>
2023-07-07 00:16:07 +0900

    ChatGPTの出力を実装（未完成）

--- oprnAi.js
+++ oprnAi.js
@@ -2,35 +2,41 @@
 // 変数の定義
 //モジュールの読み込み
 const { Configuration, OpenAIApi } = require("openai");
-const dotenv = require('dotenv').config();
+require('dotenv').config();
 const fs = require('fs');
 const http = require('http');
+
+//バックコール関数の定義
+const call = (tmp) => {
+    tmp;
+}
 
 //httpサーバーポートの設定
 const port = 8000;
 
-//環境変数の読み込み
-const env = process.env;
+//chatGPTへの入力メッセージをファイルから読み込み
+const chatGptTemplate = fs.readFileSync('./ChatGptTemplate.txt', 'utf8');
 
-//chatGPTへの入力メッセージ
-const chatGptMessage = "4問のクイズと、その答えを4択で出して";
-
-//テスト出力
+//ChatGptの各種設定
 const configuration = new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
 
-(async () => {
-    const completion = await openai.createChatCompletion({
-    model: "gpt-3.5-turbo",
-    messages: [{ role: "user", content: chatGptMessage }],
+//ChatGptの実行
+var ChatGtpAnswer = (async () => {
+        var a = await openai.createChatCompletion({
+        model: "gpt-3.5-turbo",
+        messages: [{ role: "user", content: chatGptTemplate }],
     });
-    console.log(completion.data.choices[0].message);
+    return a.data.choices[0].message;
 })();
 
 
-/**
+//テスト出力
+console.log(ChatGtpAnswer);
+
+/*
 http.createServer( (request, response)=>{
     response.writeHead(200, {'Content-Type': 'text/html'});
     response.end('Hello World');
